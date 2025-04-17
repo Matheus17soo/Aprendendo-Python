488c85c74dce6cfbc7a542032d89a9cd80051287
Matheus17soo <matheusmartins17soo@gmail.com>
2025-04-17 12:27:08 -0300

    Organizando a nomeclatura dos arquivos.

---
+++ 02 - Desafios GitHub/lista de notas 3.py
@@ -0,0 +1,7 @@
+# Como no desafio que encotrei no github, a solução usa algumas funções 
+# e maneiras diferentes para o mesmo resultado.
+# Vou refazer o segundo desafio da lista de notas, aplicando as seguintes funções.
+# While, len()
+
+
+# Autor: Matheus Martins
