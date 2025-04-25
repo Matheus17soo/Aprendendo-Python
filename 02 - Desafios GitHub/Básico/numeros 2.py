80396f7976dc3e0bfa4981a818fbe701df560ce2
Matheus17soo <matheusmartins17soo@gmail.com>
2025-04-25 12:06:45 -0300

    Correção: o desafio 3 não havia sido feito

--- 02 - Desafios GitHub/Básico/numeros 2.py
+++ 02 - Desafios GitHub/Básico/numeros 2.py
@@ -2,5 +2,5 @@
 # e que apresente o número informado com duas casas decimais
 # pra ficar mais divertido use float, pra receber qualquer número.
 
-numero = int(input('Informe um número inteiro qualquer: '))
+numero = float(input('Informe um número inteiro qualquer: '))
 print(f'Este número com duas casas decimais fica assim: {numero:.2f}')
--- 02 - Desafios GitHub/Básico/numeros 3.py
+++ 02 - Desafios GitHub/Básico/numeros 3.py
@@ -1,1 +1,8 @@
 # Desenvolva um programa que leia quatro notas e que apresente a média final.
+
+notas = []
+for i in range(4):
+    nota = float(input(f'Informe a {i+1}ª nota: '))
+    notas.append(nota)
+media = sum(notas) / len(notas)
+print(f'A média final é: {media:.2f}')
